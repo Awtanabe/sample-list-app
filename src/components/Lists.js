b87a53c34db409a431424b062432ff144c97ed99
Awtanabe <panser098@gmail.com>
2020-11-23 21:53:00 +0900

    listsコンポーネント作成

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,10 +1,23 @@
 import React, {Component} from "react"
 
 class Lists extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lists: [1,2,3,4]
+    }
+  }
+  
   render() {
-    return(<div>hello world</div>)
+    const { lists } = this.state
+    return(
+      <div>
+        <ul>
+          {lists.map((list) => <li>{list}</li>)}
+        </ul>
+      </div>
+    )
   }
 }
-
 
 export default Lists;
