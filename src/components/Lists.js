e15a1f056c47d93752f63ecb0c60189d3914318b
Awtanabe <panser098@gmail.com>
2020-11-23 22:02:16 +0900

    listsを増やす

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -7,12 +7,20 @@ class Lists extends Component {
       lists: [1,2,3,4]
     }
   }
+
+  addList() {
+    const newLists = [...this.state.lists]
+    const addNum = this.state.lists.length + 1
+    newLists.push(addNum)
+    this.setState({lists: newLists})
+  }
   
   render() {
     const { lists } = this.state
     return(
       <div>
         <ul>
+          <button onClick={this.addList.bind(this)}>増やす</button>
           {lists.map((list) => <li>{list}</li>)}
         </ul>
       </div>
